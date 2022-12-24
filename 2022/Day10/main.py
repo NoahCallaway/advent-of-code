046489783c684d5dacc0391d0d30cc5aed2ec4e0
NoahCallaway <24882928+NoahCallaway@users.noreply.github.com>
2022-12-24 12:49:11 +0000

    Day10 pt2

--- 2022/Day10/main.py
+++ 2022/Day10/main.py
@@ -1,20 +1,35 @@
-file_path = "test.txt"
+file_path = "data.txt"
 
 data = [[i for i in line.split(' ')] for line in open(file_path).read().splitlines()]
 
 x = 1
 cycle_count = 0
 signal_strength = 0
-row = ''
+row = []
 
 check_interval = [ 20, 60, 100, 140, 180, 220 ]
+row_ends = [ 40, 80, 120, 160, 200, 240 ]
+current_offset = 0
+
+def render():
+    global row, current_offset
+    if cycle_count - 1 in row_ends:
+        current_offset = cycle_count - 1
+        print(''.join(row))
+        row = []
+
+    if (cycle_count - 1) - current_offset in range(x-1,x+2):
+        row.append('#')
+    else:
+        row.append('.')
+
 
 def cycle():
     global cycle_count, signal_strength
     cycle_count += 1
     if cycle_count in check_interval:
-        print(cycle_count, x, x * cycle_count)
         signal_strength += x * cycle_count
+    render()
 
 for i in data:
     if len(i) > 1:
